b7d9580e9921b533888a96c7d6894de94c2916eb
bvk12 <krishnavenkata070@gmail.com>
2023-03-22 16:57:35 +0530

    replace provider

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "4.59.0"
     }
   }
 }
+
 provider "aws" {
   region = "ap-south-1"
-  #profile = "default"
 }
