3a0ac20a79695ddfb44287a6ffa3d47a147dd441
bvk12 <krishnavenkata070@gmail.com>
2023-03-24 17:54:09 +0530

    change us

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                    = var.image_id     #"ami-09d56f8956ab235b3"
   instance_type          = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                  = var.inst_count
-  availability_zone      = "ap-south-1a"
+  availability_zone      = "us-east-1a"
   key_name               = "kk"
   #vpc_security_group_ids = [sg-0e7597c4247e7e9d4]
 
